5b89f15d3398e00607081d96d54b5d6bf6da9a68
motokovil <65578314+motokovil@users.noreply.github.com>
2020-08-30 12:18:01 -0500

    ok

--- src/components/messageItem.js
+++ src/components/messageItem.js
@@ -2,27 +2,15 @@ import React,{useState} from 'react'
 import moment from "moment"
 //MATERIAL IU
 import {IconButton, Icon, Typography} from "@material-ui/core"
-import Button from '@material-ui/core/Button';
-import Menu from '@material-ui/core/Menu';
-import MenuItem from '@material-ui/core/MenuItem';
-import Card from '@material-ui/core/Card';
-import CardHeader from '@material-ui/core/CardHeader';
 import Avatar from '@material-ui/core/Avatar';
-import MoreVertIcon from '@material-ui/icons/MoreVert';
-
 import { makeStyles } from '@material-ui/core/styles';
-import ListSubheader from '@material-ui/core/ListSubheader';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 import Collapse from '@material-ui/core/Collapse';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
-import DraftsIcon from '@material-ui/icons/Drafts';
-import SendIcon from '@material-ui/icons/Send';
 import ExpandLess from '@material-ui/icons/ExpandLess';
 import ExpandMore from '@material-ui/icons/ExpandMore';
-import StarBorder from '@material-ui/icons/StarBorder';
 
 //REDUX
 import store from "../redux/store";
@@ -58,14 +46,9 @@ export default function MItem(props){
     //MUI
     const classes = useStyles();
     const [open, setOpen] = useState(false);
-    const [anchorEl, setAnchorEl] = React.useState(null);
 
     const handleClick = () => {
         setOpen(!open);
-    };
-
-    const handleClose = () => {
-        setAnchorEl(null);
     };
     
     return(
@@ -100,58 +83,5 @@ export default function MItem(props){
         </List>
       </Collapse>
     </div>
-
-        //<Card className={classes.root}>
-        //    <CardHeader
-        //        avatar={
-        //            <Avatar arial-label="recipe" className={classes.avatar}>
-        //                L
-        //            </Avatar>
-        //        }
-        //        action={
-        //            <div>
-        //              <Button 
-        //                aria-controls="simple-menu" 
-        //                aria-haspopup="true" 
-        //                onClick={handleClick}>
-        //                <MoreVertIcon></MoreVertIcon>
-        //              </Button>
-        //              <Menu
-        //                id="simple-menu"
-        //                anchorEl={anchorEl}
-        //                keepMounted
-        //                open={Boolean(anchorEl)}
-        //                onClose={handleClose}
-        //              >
-        //                <MenuItem onClick={() => store.dispatch(delete_(props.index))}>Eliminar mensaje</MenuItem>
-        //              </Menu>
-        //            </div>
-        //        }
-        //        title={props.content}
-        //        subheader={moment(props.date).calendar()}
-        //    />
-        //</Card>
-        //<List>
-        //    <ListItem
-        //        dense={true}
-        //        bgcolor="primary"
-        //        button onClick={handleClick}
-        //        className={classes.item}
-        //    >
-        //      <ListItemText primary={props.content} /> 
-        //      {open ? <ExpandLess /> : <ExpandMore />}
-        //    </ListItem>
-//
-        //    <Collapse in={open} timeout="auto" unmountOnExit>
-        //        <List component="div" disablePadding onClick={() => store.dispatch(delete_(props.index))}>
-        //            <ListItem button className={classes.nested}>
-        //                <ListItemIcon>
-        //                    <Icon>delete</Icon>
-        //                </ListItemIcon>
-        //                <ListItemText primary="Eliminar Mensaje" />
-        //            </ListItem>
-        //        </List>
-        //    </Collapse>            
-        //</List>
     )
 }
